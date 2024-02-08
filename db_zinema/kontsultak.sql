39c162ae5ead67770dbe04370ba8655ef39dbc77
in1dm3-d <mirari.casillasja@elorreita-errekamari.com>
2024-02-08 08:54:12 +0100

    kontsultak eginda

--- db_zinema/kontsultak.sql
+++ db_zinema/kontsultak.sql
@@ -28,7 +28,7 @@ ORDER BY COUNT(DISTINCT b.Bezero_id) ASC
 limit 1;
 
 
-SELECT z.izena AS 'Cine', a.izena AS 'Sala de Cine', AVG(f.Iraupena) AS 'Batez Besteko Iraupena'
+SELECT z.izena AS 'Zinema', a.izena AS 'Aretoa', AVG(f.Iraupena) AS 'Batez Besteko Iraupena'
 FROM ZINEMA z
 JOIN ARETOA a using (zinema_id)
 JOIN SAIOA s ON a.zinema_id = s.zinema_id AND a.aretoa_id = s.aretoa_id
@@ -36,28 +36,31 @@ JOIN FILMA f using (filma_id)
 GROUP BY z.izena, a.izena
 ORDER BY AVG(f.Iraupena) DESC;
 
-SELECT f.generoa AS 'Genero', COUNT(sr.sarrera_id) AS 'Sarrera Kopurua'
+SELECT f.generoa AS 'Genero', sum(e.kant) AS 'Sarrera Kopurua'
 FROM FILMA f
-LEFT JOIN SAIOA s ON f.filma_id = s.filma_id
-LEFT JOIN SARRERA sr ON s.saioa_id = sr.saioa_id
-GROUP BY f.generoa
-ORDER BY COUNT(sr.sarrera_id) DESC;
+JOIN SAIOA s using (filma_id)
+JOIN SARRERA sr using (saioa_id)
+JOIN erosketak e using (erosketak_id)
+group by f.generoa
+ORDER BY sum(e.kant) DESC;
 
 
-SELECT f.generoa AS 'Genero', f.izena AS 'Película', COUNT(sr.sarrera_id) AS 'Sarrera Kopurua'
+SELECT f.generoa AS 'Genero', f.izena AS 'Filma', e.kant AS 'Sarrera Kopurua'
 FROM FILMA f 
 LEFT JOIN SAIOA s using (filma_id)
 LEFT JOIN SARRERA sr using (saioa_id)
+LEFT JOIN erosketak e using (erosketak_id)
 GROUP BY f.generoa, f.izena
-ORDER BY COUNT(sr.sarrera_id) DESC;
+ORDER BY e.kant DESC;
 
-SELECT b.izena AS 'Bezeroa', COUNT(e.erosketak_id) AS 'Erosketa Kopurua', s.saioa_id, f.generoa AS 'Generoa'
+SELECT b.izena AS 'Bezeroa', count(e.erosketak_id) AS 'Erosketa Kopurua', f.generoa AS 'Generoa'
 FROM BEZEROA b
-LEFT JOIN EROSKETAK e using (Bezero_id)
-LEFT JOIN SARRERA sr using (Erosketak_id)
-LEFT JOIN SAIOA s using (saioa_id)
-LEFT JOIN FILMA f using (filma_id)
+JOIN EROSKETAK e using (Bezero_id)
+JOIN SARRERA sr using (Erosketak_id)
+JOIN SAIOA s using (saioa_id)
+JOIN FILMA f using (filma_id)
 GROUP BY b.Bezero_id, s.saioa_id, f.generoa
-ORDER BY COUNT(e.erosketak_id) DESC, s.saioa_id ASC
-LIMIT 3;
+ORDER BY COUNT(e.erosketak_id) DESC
+LIMIT 4;
+
 
